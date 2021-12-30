1d1b50dffb878341cd55ea75c9e9bf72e1b7e370
Marcus Lee Armistead <92822657+LeeReal12@users.noreply.github.com>
2021-12-30 11:11:48 -0600

    Add files via upload

---
+++ build/precache-manifest.fad3564468a4562871ccd5e4e68d66e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06437fd66ca9384a7b359f0f66d2898",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5624c4b9f2c55de93937",
+    "url": "/static/js/2.3e43d476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e43d476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779b2b73e90290981640",
+    "url": "/static/js/main.36878d8f.chunk.js"
+  },
+  {
+    "revision": "7d9b45b3c7c21bc99a53",
+    "url": "/static/js/runtime-main.1179b1de.js"
+  }
+]);
